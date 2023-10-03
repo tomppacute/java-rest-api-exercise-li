cfce6065f8abd86db4d0613843a5881c0360c8ae
Efua Akumanyi <64264510+efuaakum@users.noreply.github.com>
2023-10-03 09:44:56 +0000

    Seperate concerns, application and controller

--- src/main/java/com/cbfacademy/apiassessment/AppController.java
+++ src/main/java/com/cbfacademy/apiassessment/AppController.java
@@ -1,17 +1,15 @@
 package com.cbfacademy.apiassessment;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class App {
+public class AppController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+	@GetMapping
+	public String Ping() {
+		return "Ping";
 	}
 
 	@GetMapping("/greeting")
