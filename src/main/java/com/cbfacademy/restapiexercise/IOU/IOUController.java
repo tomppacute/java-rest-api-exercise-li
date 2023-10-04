35230a49d2c51957a222308d55cbdbc43d4de704
Efua Akumanyi <64264510+efuaakum@users.noreply.github.com>
2023-10-04 23:40:25 +0000

    Added GET and POST endpoints

--- src/main/java/com/cbfacademy/restapiexercise/IOU/IOUController.java
+++ src/main/java/com/cbfacademy/restapiexercise/IOU/IOUController.java
@@ -2,24 +2,43 @@ package com.cbfacademy.restapiexercise;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/ious")
 public class IOUController {
-	
-	@GetMapping("/")
+
+	private final List<IOU> ious = new ArrayList<>();
+
+	@GetMapping("/ping")
 	public String ping() {
 		return String.format("Service running successfully "+Instant.now().toString());
 	}
 
-	@GetMapping(value="/iou",produces = MediaType.APPLICATION_JSON_VALUE)
-	public IOU getIOUs() {
-		IOU transaction = new IOU ("Bob", "Mary", new BigDecimal("25.52"), Instant.now());
-		return transaction;
+	@GetMapping
+	public List<IOU> getIOUs() {
+		return ious;
+	}
+
+	@GetMapping(value="/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public IOU getIOUById(@PathVariable UUID id) {
+		return ious.stream()
+            .filter(item -> item.getId().equals(id))
+            .findFirst()
+            .orElse(null);
+	}
+
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public IOU createIOU(@RequestBody IOU item) {
+		item.setId(UUID.randomUUID());
+		item.setDateTime(Instant.now());
+        ious.add(item);
+        return item;
 	}
 
 }
