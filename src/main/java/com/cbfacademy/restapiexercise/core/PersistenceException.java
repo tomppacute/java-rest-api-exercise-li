9ab1f8fb8ab83d3197072d8a99d99778e60a75a9
Genyus <genyus@gmail.com>
2024-04-16 12:18:49 -0400

    chore: removes all initial content
    Students will now create the project using Spring Initializr

--- src/main/java/com/cbfacademy/restapiexercise/core/PersistenceException.java
+++
@@ -1,7 +0,0 @@
-package com.cbfacademy.restapiexercise.core;
-
-public class PersistenceException extends RuntimeException {
-    public PersistenceException(String message) {
-        super(message);
-    }
-}
